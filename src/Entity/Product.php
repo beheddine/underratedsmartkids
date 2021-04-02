4214f308169fff02acad1a19b9194535e72a6924
beheddine <beheddine.akermi@esprit.tn>
2021-04-02 21:36:37 +0100

    product entity

---
+++ src/Entity/Product.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $inStock;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $sizeQty = [];
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture2;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture3;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture4;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getInStock(): ?int
+    {
+        return $this->inStock;
+    }
+
+    public function setInStock(int $inStock): self
+    {
+        $this->inStock = $inStock;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSizeQty(): ?array
+    {
+        return $this->sizeQty;
+    }
+
+    public function setSizeQty(?array $sizeQty): self
+    {
+        $this->sizeQty = $sizeQty;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getPicture2(): ?string
+    {
+        return $this->picture2;
+    }
+
+    public function setPicture2(?string $picture2): self
+    {
+        $this->picture2 = $picture2;
+
+        return $this;
+    }
+
+    public function getPicture3(): ?string
+    {
+        return $this->picture3;
+    }
+
+    public function setPicture3(?string $picture3): self
+    {
+        $this->picture3 = $picture3;
+
+        return $this;
+    }
+
+    public function getPicture4(): ?string
+    {
+        return $this->picture4;
+    }
+
+    public function setPicture4(?string $picture4): self
+    {
+        $this->picture4 = $picture4;
+
+        return $this;
+    }
+}
